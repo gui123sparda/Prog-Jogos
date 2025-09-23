5140a70fa71a1d82ae2b89b93ef3878bc543cb30
Andravi <andravi75@gmail.com>
2025-09-23 16:28:24 -0300

    enemy init

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,28 +1,49 @@
 extends CharacterBody2D
 
+enum StateMachine {IDLE, WALK, ATTACK, PARRY, DEATH}
 
-@export var SPEED := 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED := 90.0
+@export var DIST_FOLLOW := 300.0
+@export var DIST_ATTACK := 80.0
 
 @onready var object_detecter := $RayCast2D as RayCast2D
 
 var direction := 1.0
+var att_power := 10	
+var health := 3
+var animation := ''
+var death := false
+var state := StateMachine.WALK
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		
-	if object_detecter.is_colliding():
-		direction = direction * -1
-		if $sprite.flip_h:
-			$sprite.flip_h = false
-		else:
-			$sprite.flip_h = true 
-		
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	match state:
+		StateMachine.IDLE:
+			pass
+		StateMachine.WALK:
+			# Add the gravity.
+			if not is_on_floor():
+				velocity += get_gravity() * delta
+				
+			if object_detecter.is_colliding():
+				direction = direction * -1
+				scale.x *= -1 
+			if direction:
+				velocity.x = direction * SPEED
+			
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+			move_and_slide()
+			
 
-	move_and_slide()
+		StateMachine.ATTACK:
+			pass
+		StateMachine.DEATH:
+			pass
+	
+	
+	
+	
+	
+	
+	
