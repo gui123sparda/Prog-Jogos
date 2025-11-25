dcf44e51d8c4ee2781c685b79a53917b1cb68e19
Andravi <andravi75@gmail.com>
2025-11-25 14:47:25 -0300

    fase 1 feita

---
+++ Assets/Teste_tranferencia/player.gd
@@ -0,0 +1,135 @@
+# Exemplo de script em GDScript
+extends CharacterBody2D
+
+@export var velocidade := 200.0
+@export var jump_velocidade := -400.0 
+@export var run_acl := 1.5
+@export var damage_power := 1
+@export var health := 3
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var attack_area: Area2D = $attack_area
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+var machine_state
+
+var gravity = 750.0
+
+var is_moving:= false
+var is_attacking := false
+var is_taking_damage := false
+var is_death := false
+
+
+func _attack() -> void:
+	pass
+
+
+func _ready() -> void:
+	machine_state = animation_tree.get("parameters/playback")
+
+
+func _flip(direcao) -> void:
+	attack_area.scale.x = direcao * -1
+	collision_shape_2d.scale.x = direcao * -1
+
+func _physics_process(delta):
+	print(is_taking_damage)
+	if is_death:
+		machine_state.travel("death")
+		
+		
+	
+	
+	
+	var direcao = Vector2.ZERO # Vetor de direção inicial
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+		
+	if not is_attacking and not is_taking_damage:
+		if Input.is_action_pressed("ui_right"):	
+			print("s")
+			machine_state.travel("walk")
+			direcao.x += velocidade
+
+		elif Input.is_action_pressed("ui_left"):
+			machine_state.travel("walk")
+			direcao.x -= velocidade
+		
+		if Input.is_action_pressed("ui_down") and is_on_floor():
+			velocity.y = jump_velocidade
+
+	if Input.is_action_pressed("ui_up") and is_on_floor() and not is_taking_damage:
+		is_attacking = true
+		machine_state.travel("attack")
+		
+		await animation_tree.animation_finished
+		
+
+
+
+		# Normaliza o vetor de direção para que não se mova mais rápido diagonalmente
+	if direcao.x != 0 or is_taking_damage:
+		direcao.x = direcao.normalized().x
+	else:
+		machine_state.travel("idle")
+	is_moving = direcao.x != 0
+	
+	if is_moving and not is_taking_damage:
+		#$sprite.flip_h = (direcao.x > 0)
+		_flip(direcao.x)
+		
+		#if  Input.is_action_pressed("ui_run"):
+			#machine_state.travel("running")
+			#velocity.x = direcao.x * velocidade * run_acl
+
+	velocity.x = direcao.x * velocidade
+
+	if not is_on_floor() and not is_taking_damage:
+		if velocity.y < 0.0:
+			machine_state.travel("jump")
+		else:
+			machine_state.travel("fall")
+
+	
+	
+	# Move o personagem e trata colisões
+	move_and_slide()
+	velocity.x = Vector2.ZERO.x
+
+
+func aplly_damege(damage_amount: int = 0) -> void:
+	if is_death or is_taking_damage:
+		return
+	
+	health -= damage_amount
+	is_taking_damage = true
+	is_attacking = false
+	
+	machine_state.travel("hit")
+	#print("tomou dano, vida restante: ", health)
+	
+	if health <= 0:
+		is_death = true
+		
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "death":
+		queue_free()
+	if anim_name == "attack":
+		is_attacking = false
+	print(anim_name)
+	
+	
+	if anim_name == "hit":
+		print("virou falso")
+		is_taking_damage = false
+		if health <= 0:
+			is_death = true
+			
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemies"):
+		#if body.name == "Goblin_enemy":
+		var _goblin = body.aplly_damege(damage_power)
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,42 +1,132 @@
 extends CharacterBody2D
 
-enum StateMachine {IDLE, WALK, ATTACK, PARRY, DEATH}
+enum StateMachine {IDLE, WALK, ATTACK, DEATH}
 
-@export var SPEED := 90.0
+@export var SPEED := -90.0
 @export var DIST_FOLLOW := 300.0
 @export var DIST_ATTACK := 80.0
+@export var cooldown_attack_init := 2.0
+@export var damage_power := 1
 
 @onready var object_detecter := $RayCast2D as RayCast2D
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var attack_raycast: RayCast2D = $attack_raycast
 
 var direction := 1.0
 var att_power := 10
 var health := 3
 var animation := ''
 var death := false
-var state := StateMachine.WALK
+var state := StateMachine.IDLE
+var machine_state
+var is_attacking := false
+var is_taking_damage := false
+var can_attack := true
+var attack_cooldown_time := 1.0
+
+
+func _ready() -> void:
+	machine_state = animation_tree.get("parameters/playback")
+
+
+func _run_physics(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if not is_attacking and not is_taking_damage:
+		if object_detecter.is_colliding() :
+			direction = direction * -1
+			scale.x *= -1
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+		if state != StateMachine.DEATH:
+			if health <= 0 :
+				state = StateMachine.DEATH
+			elif velocity.x == 0:
+				state = StateMachine.IDLE
+			elif velocity.x != 0:
+				state = StateMachine.WALK
+			move_and_slide()
+			if attack_raycast.is_colliding() and health > 0:
+				state = StateMachine.ATTACK
+
 
 func _physics_process(delta: float) -> void:
-	
-	match state:
-		StateMachine.IDLE:
-			pass
-		StateMachine.WALK:
-			# Add the gravity.
-			if not is_on_floor():
-				velocity += get_gravity() * delta
+	if health <0 :
+		is_attacking = false
+		death = true
+		state = StateMachine.DEATH
+	if not is_taking_damage:
+		match state:
+			StateMachine.IDLE:
+				velocity.x = 0
+				machine_state.travel("idle")
+				state = StateMachine.WALK
+				is_attacking = false
 				
-			if object_detecter.is_colliding():
-				direction = direction * -1
-				scale.x *= -1 
-			if direction:
-				velocity.x = direction * SPEED
+				
+			StateMachine.WALK:
+				if not death:
+					velocity.x = direction * SPEED
+					machine_state.travel("walk")
+					_run_physics(delta)
+					
+					
+				
+			StateMachine.ATTACK:
+				if not is_attacking and not death and can_attack:
+					is_attacking = true
+					can_attack = false
+					machine_state.travel("attack")
+					await animation_tree.animation_finished
+					#await get_tree().create_timer(cooldown_attack_init).timeout
+					state = StateMachine.IDLE
+					start_attack_cooldown()
+					state = StateMachine.IDLE
 			
-			else:
-				velocity.x = move_toward(velocity.x, 0, SPEED)
-			move_and_slide()
-			
+			StateMachine.DEATH:
+				if not death:
+					death = true
+					machine_state.travel("death")
+					await animation_tree.animation_finished
+					queue_free()
 
-		StateMachine.ATTACK:
-			pass
-		StateMachine.DEATH:
-			pass
+
+func aplly_damege(damage_amount: int = 0) -> void:
+	if state == StateMachine.DEATH  and is_taking_damage:
+		return
+	
+	health -= damage_amount
+	is_taking_damage = true
+	is_attacking = false
+	
+	machine_state.travel("takeDamege")
+	print(self.name + " tomou dano, vida restante: ", health)
+
+		
+func start_attack_cooldown() -> void:
+	await get_tree().create_timer(attack_cooldown_time).timeout
+	
+	#print("tempo")
+	can_attack = true
+	is_attacking = false
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "takeDamege":
+		is_taking_damage = false
+		if health <= 0:
+			state = StateMachine.DEATH
+			# Chama _physics_process para executar o estado DEATH
+			await get_tree().process_frame
+			_physics_process(get_process_delta_time())
+		else:
+			state = StateMachine.IDLE
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		var _player = body.aplly_damege(damage_power)
